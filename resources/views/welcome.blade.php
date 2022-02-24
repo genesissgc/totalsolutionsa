921c36788a2d142ad4334c1b2c610941220e3ffa
Genesis Lista <genesis_lista@sayeghwater.com>
2022-02-24 13:14:03 +0300

    Upload favicon image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
 
         <title>Total Solutions</title>
 
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('public/images/TS.ico') }}">
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
